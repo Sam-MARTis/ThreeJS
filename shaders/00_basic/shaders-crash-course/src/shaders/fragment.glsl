96017ca8ed473871973485e67c350a07a830999e
Sam-MARTis <samanthmartis@gmail.com>
2024-07-08 12:12:43 +0530

    Added multicolour sphere

--- shaders/00_basic/shaders-crash-course/src/shaders/fragment.glsl
+++ shaders/00_basic/shaders-crash-course/src/shaders/fragment.glsl
@@ -1,7 +1,9 @@
 precision mediump float;
 uniform float uTime;
 
+varying vec3 vPosition;
+
 void main() {
-	gl_FragColor = vec4( 0.0, 0.0, 1.0, 1.0 );
+	gl_FragColor = vec4(vPosition, 1.0 );
 }
 
--- shaders/00_basic/shaders-crash-course/src/shaders/vertex.glsl
+++ shaders/00_basic/shaders-crash-course/src/shaders/vertex.glsl
@@ -7,10 +7,12 @@ uniform mat4 modelViewMatrix;
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 
+varying vec3 vPosition;
 
 uniform float uTime;
 
 void main() {
+    vPosition = position;
 	gl_Position = projectionMatrix * viewMatrix*modelMatrix * vec4( position, 1.0 );
 }
 
