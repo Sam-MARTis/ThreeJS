a9626bd33b9791ce65e816ba1520fe3f8b46b94c
Sam-MARTis <samanthmartis@gmail.com>
2024-10-15 23:31:47 +0530

    Fixed vertex function

--- BufferGeometry/BufferGeometry/src/main.ts
+++ BufferGeometry/BufferGeometry/src/main.ts
@@ -25,8 +25,8 @@ const camera = new THREE.PerspectiveCamera(fov, AR, near, far);
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
-const maxx = 2;
-const maxy = 2;
+const maxx = 3;
+const maxy = 3;
 const step = 1;
 const size = 0.1;
 
@@ -52,8 +52,8 @@ for(let j=0; j< maxy; j++){
 
 for(let i = 0; i<maxx; i++){
     xVals[j].push(i)
-    yVals[j].push(i)
-    colourVals[j].push([1,1, 1])
+    yVals[j].push(j)
+    colourVals[j].push([1, 0, i/maxx])
 }
 }
 
@@ -64,8 +64,8 @@ const createVertices = (xArr: number[][], yArr: number[][]) => {
         vertices.push(xArr[j][i], yArr[j][i], 0);
         vertices.push(xArr[j][i+1], yArr[j][i+1], 0);
         vertices.push(xArr[j+1][i], yArr[j+1][i], 0);
-        vertices.push(xArr[j+1][i], yArr[j+1][i], 0);
         vertices.push(xArr[j+1][i+1], yArr[j+1][i+1], 0);
+        vertices.push(xArr[j+1][i], yArr[j+1][i], 0);
         vertices.push(xArr[j][i+1], yArr[j][i+1], 0);
     }
   }
@@ -75,7 +75,7 @@ const createColourVertices = (colourVals: number[][][]) => {
     const vertices = []
     for (let j = 0; j < colourVals.length; j++) {
         for (let i = 0; i < colourVals.length; i++) {
-            vertices.push(colourVals[i][j][0], colourVals[i][j][1], colourVals[i][j][2] );
+            vertices.push(colourVals[i][j][0], colourVals[i][j][1], colourVals[i][j][2], colourVals[i][j][0], colourVals[i][j][1], colourVals[i][j][2], colourVals[i][j][0], colourVals[i][j][1], colourVals[i][j][2]   );
         }
       }
         return new Float32Array(vertices);
@@ -89,12 +89,12 @@ console.log(v2)
 const c2 = createColourVertices(colourVals)
 
 geometry.setAttribute("position", new THREE.BufferAttribute(v2, 3));
-// geometry.setAttribute("color", new THREE.BufferAttribute(c2, 3));
+geometry.setAttribute("color", new THREE.BufferAttribute(c2, 3));
 // geometry.setIndex(new THREE.BufferAttribute(indices, 1));
 
 const material = new THREE.MeshStandardMaterial({
-//   vertexColors: true,
-color: "white",
+  vertexColors: true,
+// color: "white",
   wireframe: false,
 });
 
