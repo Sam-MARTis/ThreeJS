6b85952b92eded8d577e92e65f791459ba7a0292
Sam-MARTis <samanthmartis@gmail.com>
2024-10-16 18:12:55 +0530

    Fixed vertices and colour functions

--- BufferGeometry/BufferGeometry/src/main.ts
+++ BufferGeometry/BufferGeometry/src/main.ts
@@ -10,9 +10,7 @@ const makeRender = (t = 0) => {
   controls.update();
   requestAnimationFrame(makeRender);
 };
-const displacementFunction = (x: number, y: number) => {
-  return { x: x + y / 10, y: y + x / 10 };
-};
+
 
 
 
@@ -25,13 +23,13 @@ canvas.height = window.innerHeight * devicePixelRatio;
 const fov = 75;
 const AR = canvas.width / canvas.height;
 const near = 0.1;
-const far = 5;
+const far = 500;
 const camera = new THREE.PerspectiveCamera(fov, AR, near, far);
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
-const maxx = 9;
-const maxy = 2;
+const maxx = 5;
+const maxy = 5;
 const step = 1;
 const size = 0.1;
 
@@ -61,11 +59,13 @@ for(let i = 0; i<maxx; i++){
     colourVals[j].push([1, 0, i/maxx])
 }
 }
+console.log("xvals: ", xVals.length, xVals[0].length)
+console.log("yVals: ", yVals.length, yVals[0].length)
 
 const createVertices = (xArr: number[][], yArr: number[][]) => {
   const vertices = [];
   for (let j = 0; j < yArr.length-1; j++) {
-    for (let i = 0; i < xArr.length-1; i++) {
+    for (let i = 0; i < xArr[0].length-1; i++) {
         vertices.push(xArr[j][i], yArr[j][i], 0);
         vertices.push(xArr[j][i+1], yArr[j][i+1], 0);
         vertices.push(xArr[j+1][i], yArr[j+1][i], 0);
@@ -79,8 +79,10 @@ const createVertices = (xArr: number[][], yArr: number[][]) => {
 const createColourVertices = (colourVals: number[][][]) => {
     const vertices = []
     for (let j = 0; j < colourVals.length; j++) {
-        for (let i = 0; i < colourVals.length; i++) {
-            vertices.push(colourVals[i][j][0], colourVals[i][j][1], colourVals[i][j][2], colourVals[i][j][0], colourVals[i][j][1], colourVals[i][j][2], colourVals[i][j][0], colourVals[i][j][1], colourVals[i][j][2]   );
+        for (let i = 0; i < colourVals[0].length; i++) {
+          for(let k=0; k<6; k++){
+            vertices.push(colourVals[i][j][0], colourVals[i][j][1], colourVals[i][j][2])
+          }
         }
       }
         return new Float32Array(vertices);
@@ -94,12 +96,12 @@ console.log(v2)
 const c2 = createColourVertices(colourVals)
 
 geometry.setAttribute("position", new THREE.BufferAttribute(v2, 3));
-// geometry.setAttribute("color", new THREE.BufferAttribute(c2, 3));
+geometry.setAttribute("color", new THREE.BufferAttribute(c2, 3));
 // geometry.setIndex(new THREE.BufferAttribute(indices, 1));
 
 const material = new THREE.MeshStandardMaterial({
-//   vertexColors: true,
-color: "white",
+  vertexColors: true,
+// color: "white",
   wireframe: false,
 });
 
